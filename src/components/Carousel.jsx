bcbbfbdedcac200b577e72904eb277120d73e9a1
vidit-sachan <vidit.schn@gmail.com>
2025-06-13 00:21:06 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,8 +5,10 @@ import Slider from 'react-slick';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { getData } from '../context/DataContext';
 import Category from './Category';
+import { useNavigate } from 'react-router-dom';
 
 const Carousel = () => {
+    const navigate = useNavigate()
     const { data, fetchAllProducts } = getData()
     // console.log(data)
 
@@ -57,7 +59,7 @@ const Carousel = () => {
                                     <h3 className='text-red-500 font-semibold font-sans text-sm'>Powering Your World with the Best in Electronics</h3>
                                     <h1 className='md:text-4xl text-xl font-bold uppercase line-clamp-2 md:line-clamp-3 md:w-[500px] text-white'>{item.title}</h1>
                                     <p className='md:w-[500px] line-clamp-3 text-gray-400 pr-7'>{item.description}</p>
-                                    <button className='bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 rounded-md cursor-pointer mt-2'>Shop Now</button>
+                                    <button className='bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 rounded-md cursor-pointer mt-2' onClick={() => navigate('/products')}>Shop Now</button>
                                 </div>
                                 <div>
                                     <img src={item.image} alt={item.title} className='rounded-full w-[550px] hover:scale-105 transition-all shadow-2xl shadow-red-400' />
