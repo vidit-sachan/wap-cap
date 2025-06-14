69ddc0ba8bbd743f12b36e023f52ad74e4f28fa2
vidit-sachan <vidit.schn@gmail.com>
2025-06-14 06:47:56 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,10 +41,10 @@ const Navbar = ({location, getLocation, openDropdown, setOpenDropdown}) => {
                 {/* menu section */}
                 <nav className='flex gap-7 items-center'>
                     <ul className='flex gap-7 items-center text-xl font-semibold hidden md:flex'>
-                        <NavLink to={'/'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"}cursor-pointer`}><li>Home</li></NavLink>
-                        <NavLink to={'/products'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"}cursor-pointer`}><li>Products</li></NavLink>
-                        <NavLink to={'/about'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"}cursor-pointer`}><li>About</li></NavLink>
-                        <NavLink to={'/contact'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"}cursor-pointer`}><li>Contact</li></NavLink>
+                        <NavLink to={'/'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"} cursor-pointer`}><li>Home</li></NavLink>
+                        <NavLink to={'/products'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"} cursor-pointer`}><li>Products</li></NavLink>
+                        <NavLink to={'/about'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"} cursor-pointer`}><li>About</li></NavLink>
+                        <NavLink to={'/contact'} className={({ isActive }) => `${isActive ? "border-b-3 transition-all border-red-500" : "text-black"} cursor-pointer`}><li>Contact</li></NavLink>
                     </ul>
                     <Link to={'/cart'} className='relative'>
                         <IoCartOutline className='h-7 w-7' />
