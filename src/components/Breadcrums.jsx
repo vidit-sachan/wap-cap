b4c1840e1430d42f495871f1544b7bd51425ccd3
vidit-sachan <vidit.schn@gmail.com>
2025-06-18 16:44:52 +0530

    Update Breadcrums.jsx

--- src/components/Breadcrums.jsx
+++ src/components/Breadcrums.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { ChevronLeft } from 'lucide-react'
 
 const Breadcrums = () => {
     const navigate = useNavigate()
   return (
     <div className='max-w-6xl mx-auto my-10 '>
-        <h1>
-            <span className='text-xl text-gray-700 font-semibold cursor-pointer'  onClick={()=>navigate('/')}>
-                Home/
-            </span>
-            <span className='text-xl text-gray-700 font-semibold cursor-pointer' onClick={()=>navigate('/products')}>
-                Products
-            </span>
-        </h1>
+        <button onClick={()=>navigate('/products/')} className='bg-gray-800 mb-5 text-white px-3 py-1 rounded-md cursor-pointer flex gap-1 items-center'><ChevronLeft/> Back</button>
     </div>
   )
 }
